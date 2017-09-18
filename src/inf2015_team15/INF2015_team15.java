3205f389f993f31ed39421bdecf08c84152357b5
phili <phili@MSI>
2017-09-18 12:04:51 -0400

    Base du TP1, variables déclarée.

---
+++ src/inf2015_team15/INF2015_team15.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inf2015_team15;
+
+/**
+ *
+ * @author phili
+ */
+public class INF2015_team15 {
+    // DÉCLARATION DES VARIABLES DE CLASSES
+    
+    private int typeDepartement ; // réponses possible: [0,1,2]
+    private double tauxMinHoraire;
+    private double tauxMaxHoraire;
+    
+    private int nbDroitAnciennete ;
+    private int nbDiplomes;
+    private int chargeTravail;
+    private String dateRevisionSalaire ;
+    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
